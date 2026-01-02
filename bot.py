fc87b12b2e9381956882c3ae8f2e9f1612fc09df
Yavoriv27 <yurkopolka@gmail.com>
2026-01-02 11:44:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ import time
 
 TOKEN = None
 while not TOKEN:
-    TOKEN = os.getenv("BOT_TOKEN")
+    TOKEN = os.getenv("BOT_TOKEN", "").strip('"')
     if not TOKEN:
         print("Waiting for BOT_TOKEN...")
         time.sleep(2)
-
