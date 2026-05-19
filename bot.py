d8e2d600552825a32cc335e51f7a5cf341bae9a3
Yavoriv27 <yurkopolka@gmail.com>
2026-05-19 15:17:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -982,16 +982,16 @@ async def button(update: Update, context):
 
     if q.data == "signal":
 
-    signal = generate_signal()
+        signal = generate_signal()
 
-    price_df = get_data("M1", 1)
+        price_df = get_data("M1", 1)
 
-    if not price_df.empty and "close" in price_df.columns:
+        if not price_df.empty and "close" in price_df.columns:
 
-        current_price = round(
-            price_df["close"].iloc[-1],
-            5
-        )
+            current_price = round(
+                price_df["close"].iloc[-1],
+                5
+            )
 
     else:
 
